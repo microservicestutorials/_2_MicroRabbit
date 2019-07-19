a44b20d20c2d27483bd8156a6dfb567df7159029
Jason Pace <japac74@hotmail.com>
2019-07-19 12:28:55 +0200

    Implementation of RabbitMQ Bus completed

--- MicroRabbit.Domain.Core/Bus/IEventHandler.cs
+++ MicroRabbit.Domain.Core/Bus/IEventHandler.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace MicroRabbit.Domain.Core.Bus
 {
-    //in TEvent means TAKES IN ANY TYPE OF EVENT
+    //in TEvent means THAT IT TAKES IN ANY TYPE OF EVENT
     public interface IEventHandler<in TEvent> : IEventHandler where TEvent : Event
     { 
         Task Handle(TEvent @event);
